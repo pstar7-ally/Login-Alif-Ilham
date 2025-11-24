ecf990a061864a0bb7bd3549fdd9019643843beb
Alif Ilham <liffthepstar7@gmail.com>
2025-11-24 15:17:43 +0700

    Add files via upload

---
+++ isi.js
@@ -0,0 +1,7 @@
+document.writeln("<center>");
+document.writeln("<hr width=600 color=black>");
+document.writeln("<h1> universitas komputer indonesia</h1>");
+document.writeln("<h2> FAKULTAS TEKNIK dan ILMU KOMPUTER</h2>");
+document.writeln("<h3> JURUSAN TEKNIK INFORMATIKA</H3>");
+document.writeln("<HR WIDTH=600 COLOR=Black>");
+document.writeln("</center>");
